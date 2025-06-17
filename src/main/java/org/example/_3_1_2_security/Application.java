583d33b264496a071b1f4034d81558c34814f17c
Nikolay <lugovin@gmail.com>
2025-06-17 17:45:38 +0300

    Все работает, кроме LAZY. И есть очень интересный косяк с дебагом

---
+++ src/main/java/org/example/_3_1_2_security/Application.java
@@ -0,0 +1,13 @@
+package org.example._3_1_2_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
